87b267bbb8808cf01eb156665cb838f06da610a8
HunterTwist <146719345+HunterTwist@users.noreply.github.com>
2025-12-09 12:05:57 +0100

    feat : npm run dev --> script permettant de lancer l'appli vite via navigateur + electron.

--- electron/main.cjs
+++ electron/main.cjs
@@ -1,27 +1,32 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+const isDev = !app.isPackaged;
 
 function createWindow() {
     const win = new BrowserWindow({
         width: 1200,
         height: 800,
         webPreferences: {
+            preload: path.join(__dirname, "preload.js"),
             nodeIntegration: false,
             contextIsolation: true,
         },
     });
 
-    win.loadFile(path.join(__dirname, '../dist/index.html'));
-
-    // win.webContents.openDevTools();
+    if (isDev) {
+        win.loadURL("http://localhost:5173");
+    } else {
+        win.loadFile(path.join(__dirname, "../dist/index.html"));
+    }
 }
 
 app.whenReady().then(createWindow);
 
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit();
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") app.quit();
 });
 
-app.on('activate', () => {
+app.on("activate", () => {
     if (BrowserWindow.getAllWindows().length === 0) createWindow();
 });
