c4e1165d247ab9a44e2a76b4fb5b263b68edac8b
HunterTwist <146719345+HunterTwist@users.noreply.github.com>
2026-01-05 12:24:39 +0100

    fix : App.tsx add Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 
 const App: React.FC = () => {
     return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="*" element={<Home />} />
-            </Routes>
-        </Router>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="*" element={<Home />} />
+        </Routes>
     );
 };
 
